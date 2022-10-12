c53631ef62507bf26d2fb77580c825e360dd5dab
Greg Poslov <grig.skamsk@mail.ru>
2022-10-12 22:51:05 +0500

    Remade again

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,23 +1,9 @@
 public class CreditPaymentService {
-    public double calculate12(double monthlyRate) {
-        double exponentiation = Math.pow(1 + monthlyRate, 12); // Месячная процентная ставка возведенная в степень на количество месяцев + 1
+    public double calculate(double monthsCredit) {
+        double monthlyRate = 9.99 / 12 / 100; // Месячная процентная ставка
+        double exponentiation = Math.pow(1 + monthlyRate, monthsCredit); // Месячная процентная ставка возведенная в степень на количество месяцев + 1
         double annuityRatio = (monthlyRate * exponentiation) / (exponentiation - 1); // Коэффициент аннуитета
-        double amountForOneMonth12 = annuityRatio * 1_000_000; // Аннуитетный взнос
-        return amountForOneMonth12;
+        double amountForOneMonth = annuityRatio * 1_000_000; // Аннуитетный взнос
+        return amountForOneMonth;
     }
-
-    public double calculate24(double monthlyRate) {
-        double exponentiation24 = Math.pow(1 + monthlyRate, 24); // Месячная процентная ставка возведенная в степень на количество месяцев + 1
-        double annuityRatio = (monthlyRate * exponentiation24) / (exponentiation24 - 1); // Коэффициент аннуитета
-        double amountForOneMonth24 = annuityRatio * 1_000_000; // Аннуитетный взнос
-        return amountForOneMonth24;
-    }
-
-    public double calculate36(double monthlyRate) {
-        double exponentiation = Math.pow(1 + monthlyRate, 36); // Месячная процентная ставка возведенная в степень на количество месяцев + 1
-        double annuityRatio = (monthlyRate * exponentiation) / (exponentiation - 1); // Коэффициент аннуитета
-        double amountForOneMonth36 = annuityRatio * 1_000_000; // Аннуитетный взнос
-        return amountForOneMonth36;
-    }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double monthlyRate = 9.99 / 12 / 100; // Месячная процентная ставка
-        double twelveMonths = service.calculate12(monthlyRate); // Расчёт ежемесячного платежа на 12 месяцев
-        double twentyFourMonths = service.calculate24(monthlyRate); // Расчёт ежемесячного платежа на 24 месяца
-        double thirtySixMonths = service.calculate36(monthlyRate); // Расчёт ежемесячного платежа на 36 месяцев
-        System.out.println("При сумме кредита 1000000 рублей. Сумма ежемесячного платежа составит:");
-        System.out.println("На один год: " +  twelveMonths);
-        System.out.println("На два года: " + twentyFourMonths);
-        System.out.println("На три года: " + thirtySixMonths);
+        System.out.println("Срок кредита: 1 год.Сумма кредита: 1000000 рублей. Сумма ежемесячного платежа составит:" + service.calculate(12));
+        System.out.println("Срок кредита: 2 года.Сумма кредита: 1000000 рублей. Сумма ежемесячного платежа составит:" + service.calculate(24));
+        System.out.println("Срок кредита: 3 года.Сумма кредита: 1000000 рублей. Сумма ежемесячного платежа составит:" + service.calculate(36));
     }
 }
